eebdf9124172505378e26340bf86c6153b561eb8
SeveraTheDuck <142450585+SeveraTheDuck@users.noreply.github.com>
2023-08-22 14:33:25 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,134 +1,150 @@
-// данная программа решает квадратное уравнение вида ax^2 + bx + c = 0 в действительных числах
-
 #include <stdio.h>
 #include <math.h>
+#include <assert.h>
+#include <iostream>
 
 const double EPS = 1.0e-9;
-const int SS_INF_ROOTS = 3;
 
-int LinearEquation(double b, double c, double* x1);                            // linear equation case
-int QuadraticEquation(double a, double b, double c, double* x1, double* x2);   // square equation case
-double discriminant(double a, double b, double c);                             // find discriminant
-double FirstRoot(double sqrt_D, double a, double b);                           // first root
-double SecondRoot(double sqrt_D, double a, double b);                          // second root
-int input(double* a, double* b, double* c);                                    // input coefficients
-void output(int NumberOfRoots, double x1, double x2);                          // output roots
+enum class TypeOfRoots
+{
+    SS_NO_ROOTS = 0,
+    SS_ONE_ROOT = 1,
+    SS_TWO_ROOTS = 2,
+    SS_INF_ROOTS = 3
+};
+
+TypeOfRoots SolveLinearEquation(double b, double c, double* x1);                            // linear equation case
+TypeOfRoots SolveQuadraticEquation(double a, double b, double c, double* x1, double* x2);   // square equation case
+double FindDiscriminant(double a, double b, double c);                                      // find discriminant
+double FindFirstRoot(double sqrt_D, double a, double b);                                    // first root
+double FindSecondRoot(double sqrt_D, double a, double b);                                   // second root
+bool GetInput(double* a, double* b, double* c);                                             // input coefficients
+void PrintOutput(TypeOfRoots number_of_roots, double x1, double x2);                        // output roots
 
 int main(int argc, char** argv)
 {
-    double a = nan, b = nan, c = nan;                                          // a, b, c - square equation sort of ax^2 + bx + c = 0
-    double x1 = -1, x2 = 0;                                                    // x1 & x2 - roots of the equation
-    int NumberOfRoots = 0;                                                     // number of roots
+    double a = 1, b = 2, c = 1;                                                             // a, b, c - square equation sort of ax^2 + bx + c = 0
+    double x1 = -1, x2 = 0;                                                                 // x1 & x2 - roots of the equation
+    TypeOfRoots number_of_roots = TypeOfRoots::SS_NO_ROOTS;
 
     printf("Let us solve the quadratic equation!\n");
     printf("Please, type the coefficients: ");
-    if (!input(&a, &b, &c))                                                    // input and check coefficients
+    if (!GetInput(&a, &b, &c))                                                              // input and check coefficients
     {
         printf("Please, check the input type");
         return 1;
     }
 
-    if (fabs(a) < EPS)                                                         // linear equation case
+    if (fabs(a) < EPS)                                                                      // linear equation case
     {
-        NumberOfRoots = LinearEquation(b, c, &x1);
+        number_of_roots = SolveLinearEquation(b, c, &x1);
     }
     else
     {
-        NumberOfRoots = QuadraticEquation(a, b, c, &x1, &x2);                  // square equation case
+        number_of_roots = SolveQuadraticEquation(a, b, c, &x1, &x2);                        // square equation case
     }
 
-    output(NumberOfRoots, x1, x2);
+    PrintOutput(number_of_roots, x1, x2);
     return 0;
 }
 
-double discriminant(double a, double b, double c)
+double FindDiscriminant(double a, double b, double c)
 {
     return b * b - 4 * a * c;
 }
 
-double FirstRoot(double sqrt_D, double a, double b)
+double FindFirstRoot(double sqrt_D, double a, double b)
 {
     return (-b + sqrt_D) / (2 * a);
 }
 
-double SecondRoot(double sqrt_D, double a, double b)
+double FindSecondRoot(double sqrt_D, double a, double b)
 {
     return (-b - sqrt_D) / (2 * a);
 }
 
-int LinearEquation(double b, double c, double* x1)
+TypeOfRoots SolveLinearEquation(double b, double c, double* x1)
 {
+    assert(x1 != NULL);
+
     if (fabs(b) < EPS && fabs(c) >= EPS)                                       // no roots
     {
-        return 0;
+        return TypeOfRoots::SS_NO_ROOTS;
     }
     else if (fabs(b) < EPS)                                                    // inf roots
     {
-        return SS_INF_ROOTS;
+        return TypeOfRoots::SS_INF_ROOTS;
     }
     else                                                                       // one root
     {
-        *x1 = - b / c;
-        return 1;
+        *x1 = - c / b;
+        return TypeOfRoots::SS_ONE_ROOT;
     }
 }
 
-int QuadraticEquation(double a, double b, double c, double* x1, double* x2)
+TypeOfRoots SolveQuadraticEquation(double a, double b, double c, double* x1, double* x2)
 {
     assert(x1 != NULL);
     assert(x2 != NULL);
     assert(x1 != x2);
 
-    double D = discriminant(a, b, c);                                          // no roots
+    double D = FindDiscriminant(a, b, c);                                          // no roots
     if (D <= -EPS)
     {
-        return 0;
+        return TypeOfRoots::SS_NO_ROOTS;
     }
     else if (D < EPS)                                                          // one root
     {
         *x1 = - b / (2 * a);
-        return 1;
+        return TypeOfRoots::SS_ONE_ROOT;
     }
     else                                                                       // two roots
     {
-        sqrt_D = sqrt(D);
-        *x1 = FirstRoot(sqrt_D, a, b);
-        *x2 = SecondRoot(sqrt_D, a, b);
-        return 2;
+        double sqrt_D = sqrt(D);
+        *x1 = FindFirstRoot(sqrt_D, a, b);
+        *x2 = FindSecondRoot(sqrt_D, a, b);
+        return TypeOfRoots::SS_TWO_ROOTS;
     }
 }
 
-int input(double* a, double* b, double* c)
+bool GetInput(double* a, double* b, double* c)
 {
-    if (scanf("%lf", a) != 1) return 0;
-    if (scanf("%lf", b) != 1) return 0;
-    if (scanf("%lf", c) != 1) return 0;
+    assert(a != NULL);
+    assert(b != NULL);
+    assert(c != NULL);
+    assert(a != b);
+    assert(a != c);
+    assert(b != c);
+
+    if (scanf("%lf", a) != 1) return false;
+    if (scanf("%lf", b) != 1) return false;
+    if (scanf("%lf", c) != 1) return false;
 
     assert(std::isfinite(*a));
     assert(std::isfinite(*b));
     assert(std::isfinite(*c));
 
-    assert(std::isnan(*a));
-    assert(std::isnan(*b));
-    assert(std::isnan(*c));
+    assert(!isnan(*a));
+    assert(!isnan(*b));
+    assert(!isnan(*c));
 
-    return 1;
+    return true;
 }
 
-void output(int NumberOfRoots, double x1, double x2)
+void PrintOutput(TypeOfRoots number_of_roots, double x1, double x2)
 {
-    switch(NumberOfRoots)
+    switch(number_of_roots)
     {
-        case 0:
+        case TypeOfRoots::SS_NO_ROOTS:
             printf("Cannot be solved");
             break;
-        case 1:
+        case TypeOfRoots::SS_ONE_ROOT:
             printf("The only root is %lf", x1);
             break;
-        case 2:
+        case TypeOfRoots::SS_TWO_ROOTS:
             printf("The quadratic equation has two roots:\nx1 = %lf and x2 = %lf", x1, x2);
             break;
-        case SS_INF_ROOTS:
+        case TypeOfRoots::SS_INF_ROOTS:
             printf("Infinite number of roots");
             break;
         default:
