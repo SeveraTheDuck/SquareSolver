fe467af72276d66a34856ecc798c6b01a2a588ae
SeveratheDuck <antipov.aa@phystech.edu>
2024-06-21 06:42:11 +0700

    Static functions comments

--- source/quadratic_equation.c
+++ source/quadratic_equation.c
@@ -8,17 +8,28 @@
 // Doubles comparison
 //-----------------------------------------------------------------------------
 
+/**
+ * @brief Possible results for doubles comparison
+ */
 typedef
 enum doubles_cmp_status
 {
-    DOUBLES_CMP_LESS      = -1,     ///< first less than second
+    DOUBLES_CMP_LESS      = -1,     ///< first is less than second
     DOUBLES_CMP_EQUAL     =  0,     ///< first is equal to second
-    DOUBLES_CMP_GREATER   =  1,     ///< first greater than second
+    DOUBLES_CMP_GREATER   =  1,     ///< first is greater than second
     DOUBLES_CMP_UNDEFINED =  2      ///< NaN input
 }
 doubles_cmp_status;
 
 
+/**
+ * @brief Compares two doubles
+ *
+ * @param a First  double to compare
+ * @param b Second double to compare
+ *
+ * @retval Status @see doubles_cmp_status
+ */
 static doubles_cmp_status
 CompareDoubles (const double a,
                 const double b);
@@ -32,43 +43,143 @@ CompareDoubles (const double a,
 // Static functions
 //-----------------------------------------------------------------------------
 
+/**
+ * @brief Constructor for quardatic_equation structure
+ *
+ * @param coefs Coefficients of the equation
+ * @param roots Roots of the equation
+ *
+ * @retval Pointer to quardatic_equation structure
+ * @retval NULL if allocation error occurred
+ * @retval NULL if bad input  error occurred (NaN coefficients)
+ *
+ * @details Allocates memory for quardatic_equation structure and
+ * initializes its fields with given pointers
+ * The function does not copy coefficients structure
+ */
 static quadratic_equation*
 QuadraticEquationConstructor (quadratic_equation_coefs* const coefs,
                               quadratic_equation_roots* const roots);
 
 
+/**
+ * @brief Constructor for quardatic_equation_roots structure
+ *
+ * @param x1_root First  root
+ * @param x2_root Second root
+ * @param roots_number Number of roots of the equation
+ *
+ * @retval Pointer to the structure
+ * @retval NULL if allocation error occured
+ *
+ * @details Allocates memory for quardatic_equation_roots structure and
+ * initializes its fields with given roots and number values
+ */
 static quadratic_equation_roots*
 RootsConstructor (const double x1_root,
                   const double x2_root,
                   quadratic_equation_roots_number roots_number);
 
 
+/**
+ * @brief Destructor for the quardatic_equation_roots structure
+ *
+ * @param roots Pointer to the structure
+ *
+ * @retval NULL
+ *
+ * @details Frees allocated memory, returns NULL
+ */
 static quadratic_equation_roots*
 RootsDestructor (quadratic_equation_roots* const roots);
 
 
+/**
+ * @brief The function finds roots of the equation and returns them
+ *
+ * @param coefs Coefficients of the equation
+ *
+ * @retval Pointer to the roots structure
+ * @retval NULL if allocation error occured
+ * @retval NULL if bad input  error occured (NaN coefficients)
+ *
+ * @details If a_coef == 0 calls for SolveQuadraticCase() function
+ * Otherwise, calls for SolveLinearCase() function
+ */
 static quadratic_equation_roots*
 FindRoots (const quadratic_equation_coefs* const coefs);
 
 
+/**
+ * @brief Returns quadratic_equation_roots structure with initialized fields
+ *
+ * @retval Pointer to the structure
+ * @retval NULL if allocation error occured
+ *
+ * @details Initializes fields with:
+ * x1 = NaN, x2 = NaN, roots_number = QUADRATIC_EQUATION_NO_ROOTS
+ */
 static quadratic_equation_roots*
 BothCasesNoRoots (void);
 
 
 //-------------------------------------
 // Quadratic case (a_coef != 0)
+
+/**
+ * @brief Finds the roots of the quadratic equation (a_coef != 0)
+ *
+ * @param coefs Coefficients of the quadratic equation
+ *
+ * @retval Pointer to the roots structure
+ * @retval NULL if allocation error occured
+ * @retval NULL if bad input  error occured
+ *
+ * @details Finds the discriminant (D) and calls for:
+ * BothCasesNoRoots()      if D <  0
+ * QuadraticCaseOneRoot()  if D == 0
+ * QuadraticCaseTwoRoots() if D >  0
+ * Returns NULL if D is NaN (bad input)
+ */
 static quadratic_equation_roots*
 SolveQuadraticCase (const quadratic_equation_coefs* const coefs);
 
 
+/**
+ * @brief Finds discriminant of the quadratic equation
+ *
+ * @param coefs Coefficients of the quadratic equation
+ *
+ * @retval Discriminant value
+ */
 static double
 FindDiscriminant (const quadratic_equation_coefs* const coefs);
 
 
+/**
+ * @brief Returns quadratic_equation_roots structure with initialized fields
+ *
+ * @retval Pointer to the structure
+ * @retval NULL if allocation error occured
+ *
+ * @details Finds the only root value in the quadratic case
+ * and initializes fields with:
+ * x1 = value, x2 = NaN, roots_number = QUADRATIC_EQUATION_ONE_ROOT
+ */
 static quadratic_equation_roots*
 QuadraticCaseOneRoot (const quadratic_equation_coefs* const coefs);
 
 
+/**
+ * @brief Returns quadratic_equation_roots structure with initialized fields
+ *
+ * @retval Pointer to the structure
+ * @retval NULL if allocation error occured
+ *
+ * @details Finds both roots values in quadratic case
+ * and initializes fields with:
+ * x1 = value1, x2 = value2, roots_number = QUADRATIC_EQUATION_TWO_ROOTS
+ */
 static quadratic_equation_roots*
 QuadraticCaseTwoRoots (const quadratic_equation_coefs* const coefs,
                        const double discriminant);
@@ -77,14 +188,48 @@ QuadraticCaseTwoRoots (const quadratic_equation_coefs* const coefs,
 
 //-------------------------------------
 // Linear case (a_coef == 0)
+
+/**
+ * @brief Finds the roots of the linear equation (a_coef != 0)
+ *
+ * @param coefs Coefficients of the equation
+ *
+ * @retval Pointer to the roots structure
+ * @retval NULL if allocation error occured
+ * @retval NULL if bad input  error occured
+ *
+ * @details Calls for functions:
+ * BothCasesNoRoots()   if b == 0 && c != 0
+ * LiearCaseOneRoot()   if b != 0
+ * LiearCaseInfRoots()  if b == 0 && c == 0
+ */
 static quadratic_equation_roots*
 SolveLinearCase (const quadratic_equation_coefs* const coefs);
 
 
+/**
+ * @brief Returns quadratic_equation_roots structure with initialized fields
+ *
+ * @retval Pointer to the structure
+ * @retval NULL if allocation error occured
+ *
+ * @details Initializes fields with:
+ * x1 = NaN, x2 = NaN, roots_number = QUADRATIC_EQUATION_INF_ROOTS
+ */
 static quadratic_equation_roots*
 LinearCaseInfRoots (void);
 
 
+/**
+ * @brief Returns quadratic_equation_roots structure with initialized fields
+ *
+ * @retval Pointer to the structure
+ * @retval NULL if allocation error occured
+ *
+ * @details Finds the only root in linear case
+ * and initializes fields with:
+ * x1 = value, x2 = NaN, roots_number = QUADRATIC_EQUATION_ONE_ROOT
+ */
 static quadratic_equation_roots*
 LinearCaseOneRoot (const quadratic_equation_coefs* const coefs);
 //-------------------------------------
